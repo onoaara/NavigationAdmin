c0dcb5d27c3f5252393a791b72cd087aadaf0c5e
Diamond <akinrodiau25@gmail.com>
2026-03-16 13:49:02 +0100

    env

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBD5WCAQGdiD3LK4Gq2p122lPVRvoWsTdU",
-  authDomain: "navigationapp-d423d.firebaseapp.com",
-  projectId: "navigationapp-d423d",
-  storageBucket: "navigationapp-d423d.appspot.com",
-  messagingSenderId: "623217451127",
-  appId: "1:623217451127:web:a2cdaf072dc2e2df42e672",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
